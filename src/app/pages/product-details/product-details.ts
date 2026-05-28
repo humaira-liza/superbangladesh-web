a4f5b949ff937c12e7b28b9fcbb2fdba40825e27
Your Name <your@email.com>
2026-05-28 22:02:32 +0600

    fix product details page

--- src/app/pages/product-details/product-details.ts
+++ src/app/pages/product-details/product-details.ts
@@ -1,20 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { CartService } from '../../services/cart';
 
 @Component({
-selector:'app-product-details',
-standalone:true,
-templateUrl:'./product-details.html'
+  selector: 'app-product-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-details.html',
+  styleUrls: ['./product-details.scss']
 })
 
-export class ProductDetails{
+export class ProductDetails implements OnInit {
 
-id:any;
+  product: any;
 
-constructor(private route:ActivatedRoute){
+  apiUrl =
+    'https://superbangladesh-api-1.onrender.com';
 
-this.id = this.route.snapshot.params['id'];
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private cart: CartService
+  ) {}
 
-}
+  ngOnInit(): void {
 
+    const id =
+      this.route.snapshot.params['id'];
+
+    this.http.get(
+
+      `${this.apiUrl}/api/products/${id}`
+
+    ).subscribe({
+
+      next: (res: any) => {
+
+        console.log(
+          'PRODUCT DETAILS:',
+          res
+        );
+
+        this.product = res;
+      },
+
+      error: (err) => {
+
+        console.log(
+          'PRODUCT ERROR:',
+          err
+        );
+      }
+    });
+  }
+
+  addToCart(): void {
+
+    this.cart.add(this.product);
+
+    alert('Added to cart ✅');
+  }
 }
