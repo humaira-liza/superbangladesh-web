80fa08693f7bcdfd91ec7c9c8e18f90f41d09f10
Your Name <your@email.com>
2026-07-21 16:47:15 +0600

    Update desktop account menu and profile page

--- src/app/pages/account/account.ts
+++ src/app/pages/account/account.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import {
   AppLanguage,
@@ -17,12 +18,13 @@ import {
   templateUrl: './account.html',
   styleUrl: './account.css'
 })
-export class Account {
+export class Account implements OnInit {
 
   constructor(
-    private router: Router,
-    public languageService: LanguageService
-  ) {}
+  private router: Router,
+  private http: HttpClient,
+  public languageService: LanguageService
+) {}
 
   get selectedLanguage(): AppLanguage {
     return this.languageService.language();
@@ -31,6 +33,50 @@ export class Account {
   changeLanguage(language: AppLanguage): void {
     this.languageService.setLanguage(language);
   }
+
+  fullName = '';
+phone = '';
+email = '';
+address = '';
+ngOnInit(): void {
+  this.loadProfile();
+}
+
+loadProfile(): void {
+
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return;
+  }
+
+  const headers = new HttpHeaders({
+    Authorization: `Bearer ${token}`
+  });
+
+  this.http.get<any>(
+    'http://localhost:8080/api/users/profile',
+    { headers }
+  ).subscribe({
+
+  next: (user) => {
+
+  console.log('Profile API Response:', user);
+
+  this.fullName = user.fullName || '';
+  this.phone = user.phone || '';
+  this.email = user.email || '';
+  this.address = user.address || '';
+
+},
+
+    error: (err) => {
+      console.error(err);
+    }
+
+  });
+
+}
 
   logout(): void {
 
